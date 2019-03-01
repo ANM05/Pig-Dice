8f358d8011aaf4e97b75d757f6ef783e9d914087
Antony Nganga <antonynganga05@gmail.com>
2019-03-01 16:50:57 +0300

    Creates resources needed by the game and creates index.html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ $("#startbtn").click(function(){
+ alert("This isnt working");
+ });
+});
+/*function Player(name,score,roundTotal,totalScore){
+   this.name = name;
+   this.score = score;
+   this.roundTotal = roundTotal;
+   this.totalScore = totalScore;
+ }
+*/
