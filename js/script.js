3bcbed7efe00c7a1cef0ae452441a2874ae6202b
Antony Nganga <antonynganga05@gmail.com>
2019-03-01 21:40:56 +0300

    Adds functionality to start button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-
-
-$(document).ready(function(){
-  $("#startbtn").click(function(){
-  alert("This isnt working");
-  });
-});
-/*function Player(name,score,roundTotal,totalScore){
+function Player(name,score,roundTotal,totalScore){
    this.name = name;
    this.score = score;
    this.roundTotal = roundTotal;
    this.totalScore = totalScore;
- }
-*/
+ };
+
+ var player1 ="";
+ var player2 ="";
+
+ function diceRoll(){
+   Math.floor((Math.random()*6)+1);
+ };
+$(document).ready(function(){
+  $("#startbtn").click(function(){
+  player1 = new Player;
+  player2 = new Player;
+  this.name = $("input#player1Name").val();
+  this.name =$("input#player2Name").val();
+  $("#name1Out").text(this.name);
+  $("#name2Out").text(this.name);
+  });
+});
