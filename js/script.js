bc99c03e9f2cdf83f6e2c959d3bce811713555cc
Antony Nganga <antonynganga05@gmail.com>
2019-03-04 12:29:56 +0300

    Adds the hold functionality for each player

--- js/script.js
+++ js/script.js
@@ -17,7 +17,8 @@ function Player(name,){
   this.roundTotal += this.score;  
    }
     else{
-      this.roundTotal = 0
+      this.roundTotal = 0;
+      this.roundTotal = 0;
      $("#player1Roll").hide();
      $("#player2Roll").show();
      $("#message1").text("Oops!! you rolled a ONE!!");     
@@ -29,15 +30,22 @@ function Player(name,){
  this.roundTotal += this.score;  
   }
    else{
-     this.roundTotal = 0
+     this.roundTotal = 0;
+     this.totalScore = 0;
     $("#player2Roll").hide();
     $("#player1Roll").show();
     $("#message2").text("Oops!! you rolled a ONE!!");     
   } 
 };
 
-Player.prototype.hold = function(){
-  return this.roundTotal + this.totalScore;
+Player.prototype.hold1 = function(){
+  $("#player2Roll").show();
+  return this.totalScore+=this.roundTotal;  
+}
+
+Player.prototype.hold2 = function(){
+  $("#player1Roll").show();
+  return this.totalScore+=this.roundTotal;  
 }
 
 
@@ -55,7 +63,7 @@ $(document).ready(function(){
      player1.roll1();
      $("#scoreOutput").text(player1.score);
      $("#roundOutput").text(player1.roundTotal);
-     $("#totalOutput").text(player1.totalScore);
+     
      
   });
   $("#player2Roll").click(function(){
@@ -63,11 +71,16 @@ $(document).ready(function(){
     player2.roll2();
     $("#score2").text(player2.score);
     $("#round2").text(player2.roundTotal);
-    $("#total2").text(player2.roundTotal);
+  
   });
 
   $("#player1Hold").click(function(){
-    player1.hold();
+    player1.hold1();
     $("#totalOutput").text(player1.totalScore);
-  })
+  });
+
+  $("#player2Hold").click(function(){
+   player2.hold2();
+   $("#total2").text(player2.totalScore);
+  });
 });
